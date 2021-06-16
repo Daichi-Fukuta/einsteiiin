06f1642d2968b4e5add280ce23a9729d45f359ca
Daichi Fukuta <ddd.ddd777@gmail.com>
2021-06-16 21:18:38 +0900

    ogptestビューの追加

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('ogptest', function() {
+    return view('ogptest');
+});
+
 Route::get('welcome', 'WelcomeController@welcome')->name('welcome');
 Auth::routes();
 

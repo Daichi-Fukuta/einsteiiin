993ff2fae9693e13bf1cd60aec731c51b69d5f1a
Daichi Fukuta <ddd.ddd777@gmail.com>
2021-06-16 22:09:38 +0900

    mintとまったく同じ構成

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,11 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="description" content="自分の印象をフォロワーに簡単に聞けるサービス">
     @yield('ogp')
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" integrity="sha384-ujbKXb9V3HdK7jcWL6kHL1c+2Lj4MR4Gkjl7UtwpSHg/ClpViddK9TI7yU53frPN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    @hasSection('title')
+        <title>@yield('title') | {{ config('app.name') }}</title>
+    @else
+        <title>{{ config('app.name') }}</title>
+    @endif
 </head>
 <body>
-    from layout
+    from layout2
     @yield('main')
 </body>
 </html>
--- resources/views/ogptest.blade.php
+++ resources/views/ogptest.blade.php
@@ -1,8 +1,5 @@
-@extends('layout')
+@extends('Layout.main')
 @section('ogp')
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta property="og:site_name" content="Mint"/>
     <meta property="og:type" content="website"/>
     <meta property="og:title" content="Mint"/>
@@ -14,19 +11,8 @@
     <meta name="twitter:title" content="Mint"/>
     <meta name="twitter:description" content="自分の印象をフォロワーに簡単に聞けるサービス"/>
     <meta name="twitter:image" content="https://res.cloudinary.com/dajbcpqeh/image/upload/l_text:Sawarabi%20Gothic_50_bold:これはOGPテキストです！,co_rgb:333,w_500,c_fit/c_scale,h_420/v1623414920/ogp_ibkw4a.png"/>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script defer src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" integrity="sha384-ujbKXb9V3HdK7jcWL6kHL1c+2Lj4MR4Gkjl7UtwpSHg/ClpViddK9TI7yU53frPN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-    @hasSection('title')
-        <title>@yield('title') | {{ config('app.name') }}</title>
-    @else
-        <title>{{ config('app.name') }}</title>
-    @endif
-    <meta name="description" content="自分の印象をフォロワーに簡単に聞けるサービス">
 @endsection
-<body>
-    @section('main')
-        <h1>ogptest</h1>
-    @endsection
-</body>
-</html>
+@section('main')
+    hogehoge
+@endsection
+

080a51dc2cd4950793d79814a588d443905b286f
Daichi Fukuta <ddd.ddd777@gmail.com>
2021-06-16 22:31:51 +0900

    heroku 投稿用に編集

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="自分の印象をフォロワーに簡単に聞けるサービス">
+    {{-- <meta name="description" content="自分の印象をフォロワーに簡単に聞けるサービス"> --}}
+    <meta name="description" content="デスクリプション">
     @yield('ogp')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script defer src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" integrity="sha384-ujbKXb9V3HdK7jcWL6kHL1c+2Lj4MR4Gkjl7UtwpSHg/ClpViddK9TI7yU53frPN" crossorigin="anonymous"></script>
--- resources/views/ogptest.blade.php
+++ resources/views/ogptest.blade.php
@@ -1,6 +1,17 @@
 @extends('layout')
 @section('ogp')
-    <meta property="og:site_name" content="Mint"/>
+    <meta property="og:site_name" content="サイト名"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="タイトル"/>
+    <meta property="og:description" content="デスクリプション"/>
+    <meta property="og:url" content="サイトURL"/>
+    <meta property="og:image" content="https://res.cloudinary.com/dajbcpqeh/image/upload/l_text:Sawarabi%20Gothic_50_bold:これはOGPテキストです！,co_rgb:333,w_500,c_fit/c_scale,h_420/v1623414920/ogp_ibkw4a.png"/>
+    <meta property="og:locale" content="ja_JP"/>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:title" content="タイトル"/>
+    <meta name="twitter:description" content="デスクリプション"/>
+    <meta name="twitter:image" content="https://res.cloudinary.com/dajbcpqeh/image/upload/l_text:Sawarabi%20Gothic_50_bold:これはOGPテキストです！,co_rgb:333,w_500,c_fit/c_scale,h_420/v1623414920/ogp_ibkw4a.png"/>
+    {{-- <meta property="og:site_name" content="Mint"/>
     <meta property="og:type" content="website"/>
     <meta property="og:title" content="Mint"/>
     <meta property="og:description" content="自分の印象をフォロワーに簡単に聞けるサービス"/>
@@ -10,7 +21,7 @@
     <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:title" content="Mint"/>
     <meta name="twitter:description" content="自分の印象をフォロワーに簡単に聞けるサービス"/>
-    <meta name="twitter:image" content="https://res.cloudinary.com/dajbcpqeh/image/upload/l_text:Sawarabi%20Gothic_50_bold:これはOGPテキストです！なんでだよおおおおおおおおおおおおおおおおおううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううう,co_rgb:333,w_500,c_fit/c_scale,h_420/v1623414920/ogp_ibkw4a.png"/>
+    <meta name="twitter:image" content="https://res.cloudinary.com/dajbcpqeh/image/upload/l_text:Sawarabi%20Gothic_50_bold:これはOGPテキストです！なんでだよおおおおおおおおおおおおおおおおおううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううううう,co_rgb:333,w_500,c_fit/c_scale,h_420/v1623414920/ogp_ibkw4a.png"/> --}}
 @endsection
 @section('main')
     hogehoge

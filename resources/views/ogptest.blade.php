44e6e3c4c5d77718aa87b8a3e2e8005863ebf76b
Daichi Fukuta <ddd.ddd777@gmail.com>
2021-06-16 21:23:50 +0900

    ogptest2

--- resources/views/ogptest.blade.php
+++ resources/views/ogptest.blade.php
@@ -15,6 +15,15 @@
     <meta name="twitter:title" content="Mint"/>
     <meta name="twitter:description" content="自分の印象をフォロワーに簡単に聞けるサービス"/>
     <meta name="twitter:image" content="https://res.cloudinary.com/dajbcpqeh/image/upload/l_text:Sawarabi%20Gothic_50_bold:これはOGPテキストです！,co_rgb:333,w_500,c_fit/c_scale,h_420/v1623414920/ogp_ibkw4a.png"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" integrity="sha384-ujbKXb9V3HdK7jcWL6kHL1c+2Lj4MR4Gkjl7UtwpSHg/ClpViddK9TI7yU53frPN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    @hasSection('title')
+        <title>@yield('title') | {{ config('app.name') }}</title>
+    @else
+        <title>{{ config('app.name') }}</title>
+    @endif
+    <meta name="description" content="自分の印象をフォロワーに簡単に聞けるサービス">
 </head>
 <body>
     <h1>ogptest</h1>

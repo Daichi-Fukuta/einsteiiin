bd89021422f138cf2c7f603973315ae03e6d33b4
Daichi Fukuta <ddd.ddd777@gmail.com>
2021-06-16 21:21:04 +0900

    ogptest1l

--- resources/views/ogptest.blade.php
+++ resources/views/ogptest.blade.php
@@ -1,1 +1,22 @@
-hello
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:site_name" content="Mint"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="Mint"/>
+    <meta property="og:description" content="自分の印象をフォロワーに簡単に聞けるサービス"/>
+    <meta property="og:url" content="https://mint.jp.net"/>
+    <meta property="og:image" content="https://res.cloudinary.com/dajbcpqeh/image/upload/l_text:Sawarabi%20Gothic_50_bold:これはOGPテキストです！,co_rgb:333,w_500,c_fit/c_scale,h_420/v1623414920/ogp_ibkw4a.png"/>
+    <meta property="og:locale" content="ja_JP"/>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:title" content="Mint"/>
+    <meta name="twitter:description" content="自分の印象をフォロワーに簡単に聞けるサービス"/>
+    <meta name="twitter:image" content="https://res.cloudinary.com/dajbcpqeh/image/upload/l_text:Sawarabi%20Gothic_50_bold:これはOGPテキストです！,co_rgb:333,w_500,c_fit/c_scale,h_420/v1623414920/ogp_ibkw4a.png"/>
+</head>
+<body>
+    <h1>ogptest</h1>
+</body>
+</html>
